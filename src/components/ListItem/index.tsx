983a599788a7660f4c9e12df1bdda0c609b75c32
Bruno Duarte <bruno.chen.duarte@hotmail.com>
2021-05-16 12:21:42 -0300

    add id identificantion

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -26,6 +26,7 @@ const ListItem: React.FC<Props> = ({ item, isFavoriteList }) => {
 
     return (
     <div className="box"  >
+        <p className="item-id">{item.id}</p>
         <h2>{item.title}</h2>
         <p>{item.body}</p>
         <div className="favButton" onClick={() => onClickFuncion(item.id)} >
